054e6a36d693d54ff634fb7491c7cd623e903d23
Stephen Sanchez <stephensanchez05@gmail.com>
2020-03-09 20:06:17 +0000

    first commit

---
+++ lib/pokedex/login.rb
@@ -0,0 +1,8 @@
+require "pokedex/login/version"
+
+module Pokedex
+  module Login
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pokedex/login/version.rb
@@ -0,0 +1,5 @@
+module Pokedex
+  module Login
+    VERSION = "0.1.0"
+  end
+end
